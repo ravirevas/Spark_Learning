7cba0479275ba5bf4984a484c7e61b4cad097edd
ravirevas <ravir892@gmail.com>
2018-11-26 16:19:59 +0530

    save my progress

--- spark_first_rdd.py
+++ spark_first_rdd.py
@@ -1,8 +1,13 @@
 import os
 from pyspark import SparkContext,SparkConf
 os.environ['PYSPARK_PYTHON']="C:\\Users\\Ravi\\AppData\\Local\\Programs\\Python\\Python36-32\\python"
-os.environ['SPARK_HOME'] ="C:\\Users\\Ravi\\Downloads\\spark-2.3.2-bin-hadoop2.7\\spark-2.3.2-bin-hadoop2.7\\spark-2.3.2-bin-hadoop2.7"
-
+#os.environ['SPARK_HOME'] ="C:\\Users\\Ravi\\Downloads\\spark-2.3.2-bin-hadoop2.7\\spark-2.3.2-bin-hadoop2.7\\spark-2.3.2-bin-hadoop2.7"
+os.environ['SPARK_HOME'] ="C:\\Users\\Ravi\\Downloads\\spark-2.4.0-bin-hadoop2.7\\spark-2.4.0-bin-hadoop2.7"
 conf= SparkConf().setMaster("local").setAppName("MYapp")
-
 sc = SparkContext(conf = conf)
+a = ["foo", "bar", "baz"]
+save=((list, a))
+print (save)
+
+
+
