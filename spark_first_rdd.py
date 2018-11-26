87e1c0bd44b2e7133839a17f9454a9078d1579cb
ravirevas <ravir892@gmail.com>
2018-11-26 17:16:45 +0530

    save my progress

--- spark_first_rdd.py
+++ spark_first_rdd.py
@@ -1,13 +1,14 @@
 import os
 from pyspark import SparkContext,SparkConf
 os.environ['PYSPARK_PYTHON']="C:\\Users\\Ravi\\AppData\\Local\\Programs\\Python\\Python36-32\\python"
-#os.environ['SPARK_HOME'] ="C:\\Users\\Ravi\\Downloads\\spark-2.3.2-bin-hadoop2.7\\spark-2.3.2-bin-hadoop2.7\\spark-2.3.2-bin-hadoop2.7"
-os.environ['SPARK_HOME'] ="C:\\Users\\Ravi\\Downloads\\spark-2.4.0-bin-hadoop2.7\\spark-2.4.0-bin-hadoop2.7"
-conf= SparkConf().setMaster("local").setAppName("MYapp")
-sc = SparkContext(conf = conf)
-a = ["foo", "bar", "baz"]
-save=((list, a))
-print (save)
+os.environ['SPARK_HOME'] ="C:\\Users\\Ravi\\Downloads\\spark-2.3.2-bin-hadoop2.7\\spark-2.3.2-bin-hadoop2.7\\spark-2.3.2-bin-hadoop2.7"
+#os.environ['SPARK_HOME'] ="C:\\Users\\Ravi\\Downloads\\spark-2.4.0-bin-hadoop2.7\\spark-2.4.0-bin-hadoop2.7"
+
+
+logFile = "C:\\Users\\Ravi\\Desktop\\logs.txt"
+sc = SparkContext("local", "first app")
+logData = sc.textFile(logFile)
+print(logData.collect())
 
 
 
