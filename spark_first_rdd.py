d3c9cbfb174e79794b1a094d8080e61e11790c27
ravirevas <ravir892@gmail.com>
2018-11-26 14:53:21 +0530

    save my progress

---
+++ spark_first_rdd.py
@@ -0,0 +1,8 @@
+import os
+from pyspark import SparkContext,SparkConf
+os.environ['PYSPARK_PYTHON']="C:\\Users\\Ravi\\AppData\\Local\\Programs\\Python\\Python36-32\\python"
+os.environ['SPARK_HOME'] ="C:\\Users\\Ravi\\Downloads\\spark-2.3.2-bin-hadoop2.7\\spark-2.3.2-bin-hadoop2.7\\spark-2.3.2-bin-hadoop2.7"
+
+conf= SparkConf().setMaster("local").setAppName("MYapp")
+
+sc = SparkContext(conf = conf)
