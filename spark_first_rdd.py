10b69b49a9d464df9a619c861727d74c8c302455
ravirevas <ravir892@gmail.com>
2018-11-27 16:09:45 +0530

    save my progress

--- spark_first_rdd.py
+++ spark_first_rdd.py
@@ -5,10 +5,13 @@ os.environ['SPARK_HOME'] ="C:\\Users\\Ravi\\Downloads\\spark-2.3.2-bin-hadoop2.7
 #os.environ['SPARK_HOME'] ="C:\\Users\\Ravi\\Downloads\\spark-2.4.0-bin-hadoop2.7\\spark-2.4.0-bin-hadoop2.7"
 
 
-logFile = "C:\\Users\\Ravi\\Desktop\\logs.txt"
+logFile = "C:\\Users\\Ravi\\Desktop\\forspark.txt"
 sc = SparkContext("local", "first app")
 logData = sc.textFile(logFile)
-print(logData.collect())
+logdata2=(logData.collect())
+
+
+
 
 
 
