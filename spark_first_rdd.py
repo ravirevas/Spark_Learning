754bbdd4efc2c40861dc09cc482bf64ff2777bb0
ravirevas <ravir892@gmail.com>
2018-12-03 16:27:07 +0530

    save my progres

--- spark_first_rdd.py
+++ spark_first_rdd.py
@@ -1,9 +1,20 @@
 from pyspark import SparkContext,SparkConf
-logFile = "C:\\Users\\Ravi\\Desktop\\forspark.txt"
+
+logFile = "C:\\Users\\Ravi\\PycharmProjects\\Spark_Learning\\sampledata\\AAA1_ChoiceAccounting201701010000.txt"
+
 sc = SparkContext("local", "first app")
+#sc.setLogLevel("ERROR")
 logData = sc.textFile(logFile)
-logdata2=(logData.collect())
-print(logdata2)
+#print(logData.take(1))
+line=logData.map(lambda l : l.split(","))
+total_count_before=line.count()
+new_line=line.map(lambda p : (p[1],p[2]))
+
+print("1st row is")
+print(new_line.take(3))
+
+
+
 
 
 
